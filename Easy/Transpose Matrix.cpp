af2d33dd4d345ad4e57994c53fbe8d27a55b7277
cislyt <s920198@gmail.com>
2022-06-08 20:37:47 +0800

    commit 4 problems

---
+++ Easy/Transpose Matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> rs(matrix[0].size(), vector<int>(matrix.size()));
+        
+        for (int i=0; i<matrix[0].size(); ++i)
+            for (int j=0; j<matrix.size(); ++j)
+                rs[i][j] = matrix[j][i];
+        return rs;
+    }
+};
